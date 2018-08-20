eb177ce212495482ca69a8bca6f71bfc2c17f507
anilkolupula <42468775+anilkolupula@users.noreply.github.com>
2018-08-20 17:42:04 +0530

    added comment
    accepted

--- Skandia/src/test/java/Vitalsync1/Skandia/Login.java
+++ Skandia/src/test/java/Vitalsync1/Skandia/Login.java
@@ -14,6 +14,8 @@ public static void main(String args[]) throws InterruptedException
 {
 	System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
+	//
+	
 	
 	driver.get("https://os5appqa-gl.os.zephyranywhere.com/om/login?no#");
 	 
